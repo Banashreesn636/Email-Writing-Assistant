2b10593b1c6514ef80993ef4bc426a49db9a7acc
Banashree S N <banashreebana2005@gmail.com>
2026-08-06 20:44:49 +0530

    Add files via upload

---
+++ templates.py
@@ -0,0 +1,21 @@
+templates = {
+    "Job Application":
+"""
+Write a professional job application email.
+""",
+
+    "Leave Request":
+"""
+Write a leave request email.
+""",
+
+    "Complaint":
+"""
+Write a complaint email.
+""",
+
+    "Thank You":
+"""
+Write a thank-you email.
+"""
+}
